75928c7d44b299678bddb704a4ad66d463d4afaa
VENKATESH CHAVVAKULA <47623336+venki0086@users.noreply.github.com>
2020-02-07 22:39:39 +0530

    create angular project based on given input

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const clear = require('clear');
 const chalk = require('chalk');
 const figlet = require('figlet');
 const query=require('./lib/filesgeneration')
-
+const exec=require('child_process').exec;
 // clear();
 console.log(chalk.yellow(figlet.textSync('welcome to my project',{horizontalLayout:'full'})));
 const run = async()=>{
     const data=await query.askProjecttype();
     console.log(data)
+    if(data){
+        angular(data.filename);
+    }
+}
+const angular=async(data)=>{
+exec('ng new '+data,function(error,stdOut,stdErr){
+})
+// clear();
+// process.exit();
+
 }
 run();
