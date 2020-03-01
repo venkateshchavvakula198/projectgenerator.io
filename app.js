5116e8097efa6fa93abf69c448f18abd4b563f29
VENKATESH CHAVVAKULA <47623336+venki0086@users.noreply.github.com>
2020-03-01 21:10:22 +0530

    files added

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
 const clear = require('clear');
 const chalk = require('chalk');
 const figlet = require('figlet');
-const query=require('./lib/filesgeneration')
-const exec=require('child_process').exec;
-const fs=require('fs')
-const download=require('download')
+const query = require('./lib/filesgeneration')
+const exec = require('child_process').exec;
+const filescreation=require('./lib/filescreation')
 // clear();
-console.log(chalk.yellow(figlet.textSync('welcome to my project',{horizontalLayout:'full'})));
-const run = async()=>{
-    const data=await query.askProjecttype();
+console.log(chalk.yellow(figlet.textSync('Project Generation', { horizontalLayout: 'full' })));
+const run = async () => {
+    const data = await query.askProjecttype();
     console.log(data)
-    if(data){
-        // angular(data.filename);
-        filesdownload();
+    if (data) {
+        await angular(data.filename);
     }
 }
-const angular=async(data)=>{
-exec('ng new '+data,function(error,stdOut,stdErr){
-})
-// clear();
-// process.exit();
-
-}
-const filesdownload=async()=>{
-     download('https://uc873c64a3833962e87ffd231426.dl.dropboxusercontent.com/cd/0/get/AxtULS_Sx-hus80idnfwmrXDBAe18bSp-ugFVFJZ9jNALF0SjbANUF6Txwk-4UFovVdaaRDmTfEILRMTWawsWShaaHcIi41eqATxBEWtWlpKqYmy_od9FB4IwBh00e2TOeA/file?dl=1', 'dist').then(() => {
-    console.log('done!');
-
-var AdmZip = require('adm-zip');
- 
-// reading archives
-var zip = new AdmZip("./dist/newproject.zip");
-var zipEntries = zip.getEntries();
-console.log(zipEntries,'g')
-zip.extractAllTo(/*target path*/"./dist/", /*overwrite*/true);
- });
+const angular = async (data) => {
+    // exec('ng new ' + data, function (error, stdOut, stdErr) {
+    // })
+    filescreation.projectcreation()
 }
 run();
